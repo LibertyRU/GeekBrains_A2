d56b3e84a8ba9369e602380f3b840f85b00f5a35
Dmitriy Cherkasov <lifeup@inbox.ru>
2019-07-29 09:48:55 +0300

    sms commit

--- app/src/main/java/ru/geekbrains/block_2/MainActivity.java
+++ app/src/main/java/ru/geekbrains/block_2/MainActivity.java
@@ -1,8 +1,14 @@
 package ru.geekbrains.block_2;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,6 +21,10 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final int PERMISSION_REQUEST_CODE = 10; // Этот код будет возворащаться, когда пользователь согласится
+    private String phoneNumber;
+    private String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,12 +75,18 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+        View view = findViewById(R.id.drawer_layout);
+        phoneNumber = "tel:89011112542";
+        message = "Hello!";
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        switch (id){
+        case R.id.action_SMS:
+        makeSms(phoneNumber, message);
+            Snackbar.make(view, "Сообщение отправлено!", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        default:
 
+        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -79,18 +95,49 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        View view = findViewById(R.id.drawer_layout); // получаем драувер навигацию для использования в Снэке
+        switch (id)
+            {
+                case R.id.about_me :
+                case R.id.callback_icon:
+                case R.id.nav_help:
+                default:
+                    Snackbar.make(view, "Раздел меню находится в разработке", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+            }
 
-        if (id == R.id.about_me) {
-
-        } else if (id == R.id.callback_icon) {
-
-        } else if (id == R.id.nav_help) {
-
-        }
-
-        
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) view;
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    // Запрос пермиссии для вызова
+    public void requestForCallPermission() {
+        // Можем ли мы запрашивать пермиссии, если нет, то и смысла нет запрашивать
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
+            // Запрашиваем пермиссии у пользователя
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    // Это результат запроса у пользователя пермиссии
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {   // Это та самая пермиссия, что мы запрашивали?
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Все препоны пройдены и пермиссия дана, можно делать звонок
+                makeSms(phoneNumber, message);
+            }
+        }
+    }
+
+    void makeSms(String phoneNumber, String message){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+            SmsManager.getDefault().sendTextMessage(phoneNumber, null, message, null, null);
+        } else {
+// Если пермиссии нет, то запросим у пользователя
+            requestForCallPermission();
+        }
+    }
+
 }
